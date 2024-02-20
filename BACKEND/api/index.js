daf9d31d8cdeb15da4a9da647cb0aa40952eb5b1
Trishir08 <trishirbudhiraja003@gmail.com>
2024-02-21 01:47:39 +0530

    connect-db

--- BACKEND/api/index.js
+++ BACKEND/api/index.js
@@ -1,6 +1,16 @@
+
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config()
 
 const app = express() ; 
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Connected to MONGO-DB !!") ;
+}).catch((err) => {
+    console.log(err) ; 
+}) ; 
 
 app.get("/" , (req , res) => {
     res.send("Server is Ready") ; 
