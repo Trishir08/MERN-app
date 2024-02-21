b8d116153b978a5be2e1edb5dfac90572252d47e
Trishir08 <trishirbudhiraja003@gmail.com>
2024-02-21 22:24:30 +0530

    api route

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -2,12 +2,13 @@
 import express from "express"
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
-dotenv.config()
+import authRouter from "./routes/user.auth.js"
+dotenv.config() ;
 
 const app = express() ;  
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{
-    console.log("Connected to MONGO-DB !!") ;
+    console.log("Connected to  MONGO-DB !!") ;
 }).catch((err) => {
     console.log(err) ; 
 }) ; 
@@ -17,6 +18,8 @@ app.get("/" , (req , res) => {
 }) ;
 
 const port = process.env.PORT || 3000 ; 
+app.use(express.json()) ; 
+app.use("/api/auth" , authRouter) ;
 
 app.listen(port , ()=>{
     console.log(`Server is running at port : ${port}`) ;
