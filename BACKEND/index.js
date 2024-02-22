e5fc8e750ef82764272c8e770fe7f467458fa40f
Trishir08 <trishirbudhiraja003@gmail.com>
2024-02-22 11:03:17 +0530

    create a middleware for errors

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -19,7 +19,16 @@ app.get("/" , (req , res) => {
 
 const port = process.env.PORT || 3000 ; 
 app.use(express.json()) ; 
-app.use("/api/auth" , authRouter) ;
+app.use("/api/auth" , authRouter) ; 
+app.use((err , req , res , next) => {
+    const statuscode = err.statuscode ||500 ;
+    const message = err.message || "Internal server error" ;
+    return res.status(statuscode).json({
+        success : false , 
+        statuscode , 
+        message 
+    }) ;
+})
 
 app.listen(port , ()=>{
     console.log(`Server is running at port : ${port}`) ;
