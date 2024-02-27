43577acfee9998499abd9f83b71a72ff93f3afe2
Trishir08 <trishirbudhiraja003@gmail.com>
2024-02-28 01:12:09 +0530

    "commit profile

--- FRONTEND/myapp/src/firebase.js
+++ FRONTEND/myapp/src/firebase.js
@@ -4,14 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "part3-e4551.firebaseapp.com",
-  projectId: "part3-e4551",
-  storageBucket: "part3-e4551.appspot.com",
-  messagingSenderId: "580070360801",
-  appId: "1:580070360801:web:e0e245885ceafd0b3499a0"
-};
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
